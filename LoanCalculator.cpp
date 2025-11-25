64bacdad1ef7a2051ac128b80e54d82c7c79886b
abdul muiz <abdulmuiz3009@gmail.com>
2025-11-25 20:45:09 +0500

    Initial stuff with git ignore and README

--- LoanCalculator.cpp
+++ LoanCalculator.cpp
@@ -1,181 +1,26 @@
-
-#include <iostream>
-#include <math.h>
-#include <stdexcept>
-#include <sstream>
-#include <string>
-#include <math.h>
-
-#include "LoanCalculator.h"
+#include<iostream>
 
 using namespace std;
 
-LoanCalculator::LoanCalculator() :
-  amountSet_(false),
-  initialPayment_(0.0),
-  interestSet_(false),
-  paymentSet_(false),
-  periodTotalSet_(false),
-  periodElapsedSet_(false),
-  openingFee_(0.0),
-  openingPercent_(0.0)
-{
-}
+int main(){
+    double loan_amount,interest_rate,number_of_years,total_amount,monthly_amount;
 
-//
-// The actual calculation methods
-//
+	cout<<"Enter the loan amount: ";
+	cin>>loan_amount;
+	cout<<endl;
+	cout<<"Enter the interest rate: ";
+	cin>>interest_rate;
+	cout<<endl;
+	cout<<"The number of years: ";
+	cin>>number_of_years;
+	cout<<endl;
 
-/**
- * Loan balance after n payments have been made:
- *   B_n = A*(1+i)^n - (P/i)*((1+i)^n - 1)
- */
-float LoanCalculator::calculateLoanBalance()
-{
-  if(!amountSet_ || !interestSet_ || !periodElapsedSet_ || !paymentSet_)
-  {
-    throw invalid_argument("Must set loan amount, interest, and elapsed period for this calculation" );
-  }
+	total_amount=(number_of_years*loan_amount)+(number_of_years*loan_amount*(interest_rate/100.00));
+	monthly_amount=total_amount/(number_of_years*12);
 
-  return (amount_*pow((1+interestPeriodic_), periodElapsed_)) -
-         (payment_/interestPeriodic_)*(pow((1+interestPeriodic_), periodElapsed_)-1);
-}
+	cout<<"Total amount to be paid: "<<total_amount<<endl;
+	cout<<"Total interest: "<<total_amount-(number_of_years*loan_amount)<<endl;
+	cout<<"Monthly amount to be paid: "<<monthly_amount<<endl;
 
-/**
- * Payment amount on a loan:
- *   P = i*A / (1 - (1+i)^-N)
- */
-float LoanCalculator::calculatePayment()
-{
-  if(!amountSet_ || !interestSet_ || !periodTotalSet_)
-  {
-    throw invalid_argument("Must set loan amount, interest, and total period for this calculation" );
-  }
-
-  float totalAmount = amount_ - initialPayment_;
-  totalAmount = totalAmount + openingFee_ + (totalAmount * (openingPercent_/100.0));
-
-  return (interestPeriodic_*totalAmount) /
-         (1 - pow((1+interestPeriodic_), (-1*periodTotal_)));
-}
-
-/**
- * Number of payments on a loan:
- *   N = -log(1-i*A/P) / log(1+i)
- *      (You can use any logarithm base, as long as both logs use the same base.)
- *      Aunt Sally offers to lend you $3500 at 6% for that new home theater system you want.
- *      If you pay her back $100 a month, how long will it take?
- *      Solution:  6% per year is 0.5% per month, or 0.005. P = 100 and A = 3500. N = 38.57
- */
-float LoanCalculator::calculateNumberPayments()
-{
-  if(!amountSet_ || !interestSet_ || !paymentSet_)
-  {
-    throw invalid_argument("Must set loan amount, interest, and payment for this calculation" );
-  }
-
-  return (-1.0*log10(1.0-(interestPeriodic_*amount_/payment_))) /
-         log10(1.0 + interestPeriodic_);
-}
-
-/**
- * Original loan amount:
- *   A = (P/i)*(1 - (1+i)^-N)
- */
-float LoanCalculator::calculateLoanAmount()
-{
-  if(!paymentSet_ || !interestSet_ || !periodTotalSet_)
-  {
-    throw invalid_argument("Must set payment, interest, and total period for this calculation" );
-  }
-
-  return (payment_/interestPeriodic_) *
-         (1 - pow((1+interestPeriodic_), (-1*periodTotal_)));
-}
-
-/**
- * Interest Rate:
- *   i = (((1 + P/A)^(1/q) - 1 )^q - 1)  NOTICE: This is an approximate not an exact solution
- *   where q = log(1+1/N) / log(2)
-*/
-float LoanCalculator::calculateInterestRate()
-{
-  if(!amountSet_ || !paymentSet_ || !periodTotalSet_)
-  {
-    throw invalid_argument("Must set amount, payment, and total period for this calculation" );
-  }
-
-  float q = log10(1.0 + 1.0/periodTotal_) / log10(2.0);
-  float monthlyInterest = pow((pow((1.0 + payment_/amount_), 1.0/q) -1.0), q) -1.0;
-
-  return monthlyInterest*12*100;
-}
-
-float LoanCalculator::calculateEffectiveInterestRate()
-{
-  if(!amountSet_ || !periodTotalSet_)
-  {
-    throw invalid_argument("Must set amount and total period for this calculation" );
-  }
-
-  float payment = calculatePayment();
-  float totalAmount = amount_ - initialPayment_;
-
-  float q = log10(1.0 + 1.0/periodTotal_) / log10(2.0);
-  float monthlyInterest = pow((pow((1.0 + payment/totalAmount), 1.0/q) -1.0), q) -1.0;
-
-  return monthlyInterest*12*100;
-}
-
-std::string LoanCalculator::toString()
-{
-  stringstream ss;
-
-  //ss << "LoanCalculator set values:\n";
-
-  if(amountSet_)
-  {
-    ss << "Initial Amount:      " << amount_ << "\n";
-  }
-
-  if(initialPayment_ != 0.0)
-  {
-    ss << "Initial Payment:     " << initialPayment_ << "\n";
-    ss << "Actual Loan Amount:  " << (amount_ - initialPayment_) << "\n";
-  }
-
-  if(interestSet_)
-  {
-    ss << "Yearly Interest:     " << interest_ << "%\n";
-    //ss << "Yearly Interest:     " << interest_
-    //   << "\nMonthly Interest:    " << interestPeriodic_ << "\n";
-  }
-
-  if(paymentSet_)
-  {
-    ss << "Monthly payment:     " << payment_ << "\n";
-  }
-
-  if(periodTotalSet_)
-  {
-    ss << "Loan Period:         " << periodTotal_ << " months\n";
-  }
-
-  if(periodElapsedSet_)
-  {
-    ss << "Elapsed Period:      " << periodElapsed_ << " months\n";
-  }
-
-  if(openingFee_ != 0.0)
-  {
-      ss << "Opening Fee:       " << openingFee_ << "\n";
-  }
-
-  if(openingPercent_ != 0.0)
-  {
-    ss << "Opening Fee %:       " << openingPercent_ << "% = "
-       << openingPercent_/100*(amount_ - initialPayment_) << "\n";
-  }
-
-  return ss.str();
+	return 0;
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Install mingw then open and run the following commands 
+g++ -std=c++11 LoanCalculator.cpp -o calc
+./calc -cp -a 100000 -i 12 -n 360 
